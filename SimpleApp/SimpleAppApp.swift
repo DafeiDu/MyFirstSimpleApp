aa7b8fe4d48fb13bd6399d6639cb74cacfe34372
Dafei Du <dud@oregonstate.edu>
2021-09-15 18:48:09 -0400

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Dafei Du on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
